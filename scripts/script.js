fc4a7e595168a335c02a55781a2286577dda419f
Guilherme <85587714+Guilherme-Avellar@users.noreply.github.com>
2024-05-15 19:56:35 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+
+// teste base de toda função
+function test_zeros() {
+    if((document.getElementById("firstNumber").value === "") || (document.getElementById("secondNumber").value === "")) {
+        alert("Dados em branco, favor digitar algo!")
+        return false
+    }
+
+    return true;
+}
+
+
+function somar() {
+
+    if (test_zeros()){
+        resultado = Number(document.getElementById("firstNumber").value) + Number(document.getElementById("secondNumber").value);
+        document.getElementById("resultado").innerHTML = resultado;
+    }
+}
+
+function subtrair() {
+
+    if (test_zeros()){
+        resultado = Number(document.getElementById("firstNumber").value) - Number(document.getElementById("secondNumber").value);
+        document.getElementById("resultado").innerHTML = resultado;
+    }
+}
+
+function multiplicar () {
+
+    if (test_zeros()){
+        resultado = Number(document.getElementById("firstNumber").value) * Number(document.getElementById("secondNumber").value);
+        document.getElementById("resultado").innerHTML = resultado;
+    }
+}
+
+function dividir() {
+
+    if (test_zeros()){
+
+        if (Number(document.getElementById("secondNumber").value) != 0) {
+            resultado = Number(document.getElementById("firstNumber").value) / Number(document.getElementById("secondNumber").value);
+            document.getElementById("resultado").innerHTML = resultado;
+        }
+        else {
+            alert("Não há divisão por zero");
+        }
+    }
+}
